ad031b837eeeba5b96dccb377cc740667e7f4a7d
Jack Willis <jack@attac.us>
2021-04-04 23:48:28 -0500

    Fix route names, add organization name

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'announcements#new'
   
-  get '/', action: :new, controller: :announcements, as: :announcements
-  post '/', action: :create, controller: :announcements, as: :new_announcement
-  get '/history', action: :index, controller: :announcements, as: :create_announcement
+  get '/', action: :new, controller: :announcements, as: :new_announcement
+  post '/', action: :create, controller: :announcements, as: :create_announcement
+  get '/history', action: :index, controller: :announcements, as: :announcements
 end
